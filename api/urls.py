51bae50a62dc5f06f2a105fb6e5f1b875cc06ba4
Ryan Ransom <Jhonais@Ryans-MacBook-Pro.local>
2015-11-21 17:51:18 -0800

    url fix

--- api/urls.py
+++ api/urls.py
@@ -8,11 +8,10 @@ urlpatterns = [
     url(r'^api-token-auth/', views.obtain_auth_token),
     url(r'^lists/(?P<pk>\d+)', DetailUpdateList.as_view(),
         name='api_list_detail_update'),
-    url(r'^lists/', ListCreateList.as_view(), name='api_list_list_create'),
+    url(r'^lists/$', ListCreateList.as_view(), name='api_list_list_create'),
     url(r'^items/(?P<pk>\d+)', DetailUpdateItem.as_view(),
         name='api_item_detail_update'),
-    url(r'^items/', ListCreateItem.as_view(), name='api_item_list_create'),
+    url(r'^items/$', ListCreateItem.as_view(), name='api_item_list_create'),
     url(r'^users/', ListCreateUsers.as_view(), name='api_user_list'),
-    url(r'^pledges/', ListPledge.as_view(), name='api_pledge_list'),
+    url(r'^pledges/$', ListPledge.as_view(), name='api_pledge_list'),
 ]
-urlpatterns = format_suffix_patterns(urlpatterns)
